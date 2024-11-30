2b4bbfca7af5aa0073698760d7e57ddee28fde35
Mohamad Naserzadeh <mohamad@26050012-50P3.ekrf.ir>
2024-11-30 10:54:37 +0330

    add user password to entity

--- service/userservice/service.go
+++ service/userservice/service.go
@@ -1,6 +1,8 @@
 package userservice
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 
 	"github.com/mfaxmodem/gameap/entity"
@@ -18,6 +20,7 @@ type Service struct {
 type RegisterRequest struct {
 	Name        string `json:"name"`
 	PhoneNumber string `json:"phone_number"`
+	Password    string `json:"password"`
 }
 type RegisterResponse struct {
 	User entity.User
@@ -51,12 +54,20 @@ func (s Service) Register(reg RegisterRequest) (RegisterResponse, error) {
 	if len(reg.Name) < 3 || len(reg.Name) > 50 {
 		return RegisterResponse{}, fmt.Errorf("invalid name format")
 	}
+	//TODO: check uniqueness REGEX
+	//validate password
+	if len(reg.Password) < 8 {
+		return RegisterResponse{}, fmt.Errorf("length password should be greater than 8")
+	}
+
+	//TODO: Change password to bycrypt
 
 	// create new user in storage
 	user := entity.User{
 		ID:          0,
 		PhoneNumber: reg.PhoneNumber,
 		Name:        reg.Name,
+		Password:    getMD5Hash(reg.Password),
 	}
 
 	createUser, err := s.repo.Register(user)
@@ -67,4 +78,21 @@ func (s Service) Register(reg RegisterRequest) (RegisterResponse, error) {
 
 	// return created user
 
+}
+
+type LoginRequest struct {
+	PhoneNumber string
+	Password    string
+}
+
+type LoginResponse struct {
+}
+
+func (s Service) Login(reg LoginRequest) (LoginResponse, error) {
+	panic("")
+}
+
+func getMD5Hash(text string) string {
+	hash := md5.Sum([]byte(text))
+	return hex.EncodeToString(hash[:])
 }
